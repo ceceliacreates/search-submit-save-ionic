0f83161853a59d9bb7fa09aba315b1034abc3b25
Cecelia Martinez <cecelia@cypress.io>
2021-11-08 19:58:19 -0500

    writes Search page tests

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -29,9 +29,31 @@ describe("Tab navigation", () => {
 describe("Search page", () => {
   beforeEach("it resets viewport and visits the Search tab", () => {
     cy.viewport("iphone-x");
-    cy.visit("/search");
+    cy.visit("/tabs/search");
   });
-  it("returns matching result when found", () => {});
-  it("returns error when no term entered", () => {});
-  it("returns no results when no matches found", () => {});
+  it("returns matching result when found", () => {
+    cy.get(".native-input").clear();
+    cy.get(".native-input").type("1");
+    cy.get(".button").click();
+    cy.get("ion-label").should("contain.text", "1");
+    cy.get(".list-md")
+      .children()
+      .should("have.length", 1);
+  });
+  it("returns error when no term entered", () => {
+    cy.get(".native-input").clear();
+    cy.get(".button").click();
+    cy.contains("Please enter search term");
+    cy.get(".list-md")
+      .children()
+      .should("have.length", 3);
+  });
+  it("returns no results when no matches found", () => {
+    cy.get(".native-input").clear();
+    cy.get(".native-input").type("invalid search term");
+    cy.get(".button").click();
+    cy.get(".list-md")
+      .children()
+      .should("have.length", 0);
+  });
 });
